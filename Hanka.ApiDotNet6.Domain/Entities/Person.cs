3f783afb91946bf49174cdf77a9f3e9369be4f95
Antonio Anderson <hiroto.anderson@gmail.com>
2022-11-03 11:50:17 -0300

    creating person class

---
+++ Hanka.ApiDotNet6.Domain/Entities/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Hanka.ApiDotNet6.Domain.Validations;
+
+namespace Hanka.ApiDotNet6.Domain.Entities
+{
+    /*
+        sealed não pode ser herdada 
+    */
+    public sealed class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        public Person(string document, string name, string phone)
+        {
+            Validation(document, name, phone);        
+        }
+
+        private void Validation(string document, string name, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Name cannot be null or empty");
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Document cannot be null or empty");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Document cannot be null or empty");
+
+            Name = name;
+            Document = document;
+            Phone = Phone;
+        }
+    }
+}
+
---
+++ Hanka.ApiDotNet6.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hanka.ApiDotNet6.Domain.Validations
+{
+    /* Herdando Exception */
+    public class DomainValidationException : Exception
+    {
+        public DomainValidationException(string error) : base(error)
+        { }
+
+        public static void When(bool hasError, string message)
+        {
+            if (hasError)
+                throw new DomainValidationException(message);
+        }
+    }
+}
+
