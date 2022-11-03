5434d440593c9ecd5568de39d7fcf624d0897d72
Antonio Anderson <hiroto.anderson@gmail.com>
2022-11-03 14:45:28 -0300

    mapping Person class and create DbContext

---
+++ Hanka.ApiDotNet6.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using Hanka.ApiDotNet6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hanka.ApiDotNet6.Infra.Data.Context
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+
+    /* maper entidades e informar que a mesma será uma tabela */
+    public DbSet<Person> People { get; set; }
+
+    /* Sobrescrever o método para aplicar as configurações dessa classe*/
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof (ApplicationDbContext).Assembly);
+    }
+  }
+}
---
+++ Hanka.ApiDotNet6.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,28 @@
+using Hanka.ApiDotNet6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Hanka.ApiDotNet6.Infra.Data.Maps
+{
+  public class PersonMap : IEntityTypeConfiguration<Person>
+  {
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+      // mapando a tabela e informando a chave
+      builder.ToTable("Pessoa");
+      builder.HasKey(c => c.Id);
+      builder.Property(c => c.Id)
+        .HasColumnName("IdPessoa")
+        .UseIdentityColumn();
+      builder.Property(c => c.Document)
+        .HasColumnName("Documento");
+      builder.Property(c => c.Name)
+        .HasColumnName("Nome");
+      builder.Property(c => c.Phone)
+        .HasColumnName("Celular");
+
+    }
+  }
+}
+
